366efacabcc4be9c7a92d0fd6f929e01e5241c1f
niuniuisstuding <2996674687@qq.com>
2022-03-05 12:30:20 +0800

    iftest

---
+++ hell/src/hell/ifTest.java
@@ -0,0 +1,12 @@
+package hell;
+
+public class ifTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 1;
+        if(1 == a)
+        	System.out.println("good");
+	}
+
+}
