5f86260f2467385aeb5c76368a5f4d6333cff7f5
niuniuisstuding <2996674687@qq.com>
2022-05-01 10:22:57 +0800

    完全平方数：给定m，求最小n使得m*n为完全平方

--- hell/src/chapter11Test/EX11_17.java
+++ hell/src/chapter11Test/EX11_17.java
@@ -1,5 +1,27 @@
 package chapter11Test;
-
+import java.util.Scanner;
 public class EX11_17 {
-
+	public static void main(String[] args)
+	{
+		System.out.print("Enter an integer m:");
+		Scanner input = new Scanner(System.in);
+		int m = input.nextInt(), n = 1;
+		int temp = m;
+		for(int i = 2; i <= m; i++)
+		{
+			int count = 0;
+			if(m % i == 0)
+			{
+				while(m % i == 0)
+				{
+					count++;
+					m /= i;
+				}
+				if(1 == count % 2)
+					n *= i;
+			}
+		}
+		System.out.println("The smallest number n for m * n to be a perfect square is " + n);
+		System.out.println("m * n is " + temp * n);
+	}
 }
